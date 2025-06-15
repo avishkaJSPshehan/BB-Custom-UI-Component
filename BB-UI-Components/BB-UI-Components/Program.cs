788cba1912232abcccf27579e594de9125ee62fb
JAYASIRI S.P.A.S. <ict21860@fot.sjp.ac.lk>
2025-06-15 21:10:48 +0530

    Create Custom BB Button Design

--- BB-UI-Components/BB-UI-Components/Program.cs
+++ BB-UI-Components/BB-UI-Components/Program.cs
@@ -16,7 +16,7 @@ namespace BB_UI_Components
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Sample_Form());
         }
     }
 }
