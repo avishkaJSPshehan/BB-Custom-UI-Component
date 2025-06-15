9465c7db6f545017ccc9fd1bd03adfac08a746fd
JAYASIRI S.P.A.S. <ict21860@fot.sjp.ac.lk>
2025-06-15 21:37:17 +0530

    Create BB TextBox Design

--- BB-UI-Components/BB-UI-Components/Form1.Designer.cs
+++ BB-UI-Components/BB-UI-Components/Form1.Designer.cs
@@ -28,13 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.customButton4 = new Vikum_Creation_Demo.Components.CustomButton();
+            this.customButton3 = new Vikum_Creation_Demo.Components.CustomButton();
+            this.customButton2 = new Vikum_Creation_Demo.Components.CustomButton();
+            this.customButton1 = new Vikum_Creation_Demo.Components.CustomButton();
+            this.customTextBox1 = new Vikum_Creation_Demo.Components.CustomTextBox();
+            this.customTextBox2 = new Vikum_Creation_Demo.Components.CustomTextBox();
+            this.customTextBox3 = new Vikum_Creation_Demo.Components.CustomTextBox();
             this.SuspendLayout();
+            // 
+            // customButton4
+            // 
+            this.customButton4.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.customButton4.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.customButton4.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.customButton4.BorderRadius = 20;
+            this.customButton4.BorderSize = 0;
+            this.customButton4.FlatAppearance.BorderSize = 0;
+            this.customButton4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.customButton4.ForeColor = System.Drawing.Color.White;
+            this.customButton4.Location = new System.Drawing.Point(687, 8);
+            this.customButton4.Name = "customButton4";
+            this.customButton4.Size = new System.Drawing.Size(101, 39);
+            this.customButton4.TabIndex = 3;
+            this.customButton4.Text = "Search";
+            this.customButton4.TextColor = System.Drawing.Color.White;
+            this.customButton4.UseVisualStyleBackColor = false;
+            // 
+            // customButton3
+            // 
+            this.customButton3.BackColor = System.Drawing.Color.Navy;
+            this.customButton3.BackgroundColor = System.Drawing.Color.Navy;
+            this.customButton3.BackgroundImage = global::BB_UI_Components.Properties.Resources.icon;
+            this.customButton3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.customButton3.BorderColor = System.Drawing.Color.Cyan;
+            this.customButton3.BorderRadius = 20;
+            this.customButton3.BorderSize = 0;
+            this.customButton3.FlatAppearance.BorderSize = 0;
+            this.customButton3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.customButton3.ForeColor = System.Drawing.Color.White;
+            this.customButton3.Location = new System.Drawing.Point(638, 282);
+            this.customButton3.Name = "customButton3";
+            this.customButton3.Size = new System.Drawing.Size(150, 130);
+            this.customButton3.TabIndex = 2;
+            this.customButton3.TextColor = System.Drawing.Color.White;
+            this.customButton3.UseVisualStyleBackColor = false;
+            // 
+            // customButton2
+            // 
+            this.customButton2.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.customButton2.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.customButton2.BorderColor = System.Drawing.Color.Cyan;
+            this.customButton2.BorderRadius = 20;
+            this.customButton2.BorderSize = 0;
+            this.customButton2.FlatAppearance.BorderSize = 0;
+            this.customButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.customButton2.ForeColor = System.Drawing.Color.White;
+            this.customButton2.Location = new System.Drawing.Point(174, 372);
+            this.customButton2.Name = "customButton2";
+            this.customButton2.Size = new System.Drawing.Size(120, 40);
+            this.customButton2.TabIndex = 1;
+            this.customButton2.Text = "LogIn";
+            this.customButton2.TextColor = System.Drawing.Color.White;
+            this.customButton2.UseVisualStyleBackColor = false;
+            // 
+            // customButton1
+            // 
+            this.customButton1.BackColor = System.Drawing.Color.Transparent;
+            this.customButton1.BackgroundColor = System.Drawing.Color.Transparent;
+            this.customButton1.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.customButton1.BorderRadius = 20;
+            this.customButton1.BorderSize = 3;
+            this.customButton1.FlatAppearance.BorderSize = 0;
+            this.customButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.customButton1.ForeColor = System.Drawing.Color.Black;
+            this.customButton1.Location = new System.Drawing.Point(32, 372);
+            this.customButton1.Name = "customButton1";
+            this.customButton1.Size = new System.Drawing.Size(120, 40);
+            this.customButton1.TabIndex = 0;
+            this.customButton1.Text = "Cancel";
+            this.customButton1.TextColor = System.Drawing.Color.Black;
+            this.customButton1.UseVisualStyleBackColor = false;
+            // 
+            // customTextBox1
+            // 
+            this.customTextBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.customTextBox1.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.customTextBox1.BorderSize = 2;
+            this.customTextBox1.ForeColor = System.Drawing.Color.DimGray;
+            this.customTextBox1.Location = new System.Drawing.Point(32, 306);
+            this.customTextBox1.Multiline = false;
+            this.customTextBox1.Name = "customTextBox1";
+            this.customTextBox1.Padding = new System.Windows.Forms.Padding(7);
+            this.customTextBox1.PasswordChar = false;
+            this.customTextBox1.Size = new System.Drawing.Size(262, 32);
+            this.customTextBox1.TabIndex = 4;
+            this.customTextBox1.Texts = "";
+            this.customTextBox1.UnderlinedStyle = true;
+            // 
+            // customTextBox2
+            // 
+            this.customTextBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.customTextBox2.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.customTextBox2.BorderSize = 2;
+            this.customTextBox2.ForeColor = System.Drawing.Color.DimGray;
+            this.customTextBox2.Location = new System.Drawing.Point(32, 268);
+            this.customTextBox2.Multiline = false;
+            this.customTextBox2.Name = "customTextBox2";
+            this.customTextBox2.Padding = new System.Windows.Forms.Padding(7);
+            this.customTextBox2.PasswordChar = false;
+            this.customTextBox2.Size = new System.Drawing.Size(262, 32);
+            this.customTextBox2.TabIndex = 5;
+            this.customTextBox2.Texts = "";
+            this.customTextBox2.UnderlinedStyle = true;
+            // 
+            // customTextBox3
+            // 
+            this.customTextBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.customTextBox3.BorderColor = System.Drawing.Color.MediumSlateBlue;
+            this.customTextBox3.BorderSize = 2;
+            this.customTextBox3.ForeColor = System.Drawing.Color.DimGray;
+            this.customTextBox3.Location = new System.Drawing.Point(32, 13);
+            this.customTextBox3.Multiline = false;
+            this.customTextBox3.Name = "customTextBox3";
+            this.customTextBox3.Padding = new System.Windows.Forms.Padding(7);
+            this.customTextBox3.PasswordChar = false;
+            this.customTextBox3.Size = new System.Drawing.Size(638, 32);
+            this.customTextBox3.TabIndex = 6;
+            this.customTextBox3.Texts = "";
+            this.customTextBox3.UnderlinedStyle = false;
             // 
             // Sample_Form
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.customTextBox3);
+            this.Controls.Add(this.customTextBox2);
+            this.Controls.Add(this.customTextBox1);
+            this.Controls.Add(this.customButton4);
+            this.Controls.Add(this.customButton3);
+            this.Controls.Add(this.customButton2);
+            this.Controls.Add(this.customButton1);
             this.Name = "Sample_Form";
             this.Text = "Sample Form";
             this.ResumeLayout(false);
@@ -42,6 +177,14 @@
         }
 
         #endregion
+
+        private Vikum_Creation_Demo.Components.CustomButton customButton1;
+        private Vikum_Creation_Demo.Components.CustomButton customButton2;
+        private Vikum_Creation_Demo.Components.CustomButton customButton3;
+        private Vikum_Creation_Demo.Components.CustomButton customButton4;
+        private Vikum_Creation_Demo.Components.CustomTextBox customTextBox1;
+        private Vikum_Creation_Demo.Components.CustomTextBox customTextBox2;
+        private Vikum_Creation_Demo.Components.CustomTextBox customTextBox3;
     }
 }
 
