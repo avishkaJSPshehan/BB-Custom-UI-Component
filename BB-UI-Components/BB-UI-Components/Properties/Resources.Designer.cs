21501080927a37701f9b6aee6edb62fe16b003b2
JAYASIRI S.P.A.S. <ict21860@fot.sjp.ac.lk>
2025-06-16 21:26:28 +0530

    Add custom button

--- BB-UI-Components/BB-UI-Components/Properties/Resources.Designer.cs
+++ BB-UI-Components/BB-UI-Components/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace BB_UI_Components.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap _25231 {
+            get {
+                object obj = ResourceManager.GetObject("25231", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap icon {
             get {
                 object obj = ResourceManager.GetObject("icon", resourceCulture);
